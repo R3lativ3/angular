80e52f56938de7e6b87f8b3eb0e29513299f5bbb
R3lativ3 <josebarrundia21@gmail.com>
2021-08-09 16:50:37 -0600

    lel

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -4,7 +4,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule } from '@angular/forms';
 import { PersonajesComponent } from './personajes/personajes.component';
 import { AgregarComponent } from './agregar/agregar.component';
-
+import { DbzService } from './services/dbz.service'
 
 
 @NgModule({
@@ -19,6 +19,9 @@ import { AgregarComponent } from './agregar/agregar.component';
   imports: [
     CommonModule,
     FormsModule
+  ],
+  providers:[
+    DbzService
   ]
 })
 export class DbzModule { }
